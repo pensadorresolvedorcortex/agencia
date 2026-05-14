2cae4f81658603536790ec4451eb108ab8cc6a1a
pensadorresolvedorcortex <contato@studioprivilege.com.br>
2026-05-14 14:32:01 -0300

    Redirect documentos step to dashboard in Flex assisted mode

--- wp-content/plugins/rma-flex-onboarding/rma-flex-onboarding.php
+++ wp-content/plugins/rma-flex-onboarding/rma-flex-onboarding.php
@@ -286,7 +286,8 @@ final class RMA_Flex_Onboarding {
         }
 
         $notice = isset($_GET['rma_notice']) ? sanitize_key((string) wp_unslash($_GET['rma_notice'])) : '';
-        if ($notice !== 'entity-created') {
+        $step = isset($_GET['rma_step']) ? sanitize_key((string) wp_unslash($_GET['rma_step'])) : '';
+        if ($notice !== 'entity-created' && $step !== 'documentos') {
             return;
         }
 
@@ -581,7 +582,7 @@ final class RMA_Flex_Onboarding {
                 var restBase = <?php echo wp_json_encode(rest_url('rma-flex/v1/profile-site')); ?>;
                 var dashboardShortcut = <?php echo wp_json_encode(add_query_arg('rma_assisted_skip', '1', home_url('/dashboard/'))); ?>;
                 var params = new URLSearchParams(window.location.search || '');
-                if (params.get('rma_notice') === 'entity-created') {
+                if (params.get('rma_notice') === 'entity-created' || params.get('rma_step') === 'documentos') {
                     window.location.replace(dashboardShortcut);
                     return;
                 }
